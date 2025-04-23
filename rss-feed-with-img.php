57e48227946b073a6403c942d23bf401947af41b
Abdul Majeed <52486635+majeed-ali@users.noreply.github.com>
2025-04-23 17:44:58 +0400

    Initial Upload

---
+++ rss-feed-with-img.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Featured Image in RSS Feed
+ * Description: Adds the post's featured image to the RSS feed content and excerpt.
+ * Version: 1.0.0
+ * Author: Abdul Majeed Ali
+ */
+
+// Exit if accessed directly
+defined('ABSPATH') || exit;
+
+
+require_once plugin_dir_path(__FILE__) . 'includes/loader.php';
